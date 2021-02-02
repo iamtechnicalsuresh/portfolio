79bb80cb476d7d807595b8b3d8da612e7b7b9ff3
iamtechnicalsuresh <71255609+iamtechnicalsuresh@users.noreply.github.com>
2021-02-02 17:03:38 +0530

    Change in App Js file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 const navToggle = document.querySelector(".navToggle");
 const navLink = document.querySelector(".nav-links");
+const date = document.getElementById("date")
+
+date.innerHTML = new Date().getFullYear();
+
 
 
 navToggle.addEventListener('click', ()=> {
@@ -15,5 +19,36 @@ const showNavbar = ()=> {
         navLink.classList.remove("nav-link-show");
     } else {
         navLink.classList.add("nav-link-show")
+    }
+}
+
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", ()=> {
+    fixedNav();
+    backToTop();
+})
+
+const fixedNav = ()=> {
+    const scrollPageHeight = window.pageYOffset;
+    const navbarHeight = navbar.getBoundingClientRect().height;
+    
+    if (scrollPageHeight > navbarHeight) {
+        navbar.classList.add("fixed-nav");
+    } else {
+        navbar.classList.remove("fixed-nav");
+    }
+}
+
+const backToTopBtn = document.querySelector(".back-to-top");
+const backToTop = ()=> {
+    const scrollPageHeight = window.pageYOffset;
+    console.log(scrollPageHeight)
+    if ( scrollPageHeight > 500) {
+        backToTopBtn.classList.add("back-to-top-show");
+
+    } else {
+        backToTopBtn.classList.remove("back-to-top-show")
     }
 }
